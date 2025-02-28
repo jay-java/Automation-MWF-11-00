3d6a24cd202800d79c5b7873c61da1ad806a6616
jay-java <jayprajapati.tops@gmail.com>
2025-02-28 12:32:27 +0530

    array typecating

--- CoreJAVA/src/core/P009_TypeCasting.java
+++ CoreJAVA/src/core/P009_TypeCasting.java
@@ -1,5 +1,22 @@
 package core;
 
-public class P009_TypeCasting {
+import java.util.Scanner;
 
+public class P009_TypeCasting {
+	public static void main(String[] args) {
+//		int a = 10;
+//		System.out.println(a);
+		int i = (int)12.345;
+		System.out.println(i);
+		double d = 34;//implicit
+		System.out.println(d);
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter a = ");
+		int a = sc.nextInt();
+		System.out.println("enter b = ");
+		int b = sc.nextInt();
+		double c = (double)a/(double)b;
+		System.out.println(c);
+		
+	}
 }
